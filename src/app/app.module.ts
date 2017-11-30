d56d8963ee898aae3b38d08685bf6120141710c3
Quinn O'Neill <quinnoneill@Quinns-MacBook-Pro-2.local>
2017-11-29 17:04:45 -0700

    pushing up to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { InternalDefensesModule } from './internal-defenses/internal-defenses.module'
 
 import { AppComponent } from './app.component';
+import { TieFighterComponent } from './tie-fighter/tie-fighter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TieFighterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InternalDefensesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
